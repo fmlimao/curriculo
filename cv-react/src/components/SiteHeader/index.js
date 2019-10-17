d67f577fcac3bdbd77799d71adc4adf9369b4345
Leandro Macedo <fmlimao@gmail.com>
2019-10-17 19:27:24 -0300

    Separando os componentes do Header

--- cv-react/src/components/SiteHeader/index.js
+++ cv-react/src/components/SiteHeader/index.js
@@ -4,6 +4,42 @@ import './style.css';
 
 import imagePerfil from '../../assets/img/leandro-macedo.jpg';
 
+class SiteHeaderAvatar extends React.Component {
+    render() {
+        return (
+            <div className="cv-header-avatar">
+                <img src={imagePerfil} alt="Leandro Macedo"
+                    className="img-responsive img-circle img-thumbnail" />
+            </div>
+        )
+    }
+}
+
+class SiteHeaderTitles extends React.Component {
+    render() {
+        return (
+            <div className="cv-header-titles">
+                <h1 className="cv-header-title">
+                    Leandro<br />
+                    <span className="color-a">Macedo</span>
+                </h1>
+                <h2 className="cv-header-subtitle" itemprop="jobTitle">Desenvolvedor</h2>
+            </div>
+        )
+    }
+}
+
+class SiteHeaderAddress extends React.Component {
+    render() {
+        return (
+            <address className="cv-header-address">
+                <p>Telefone: <strong className="color-a" itemprop="telephone">+55 11 98739-1075</strong></p>
+                <p>E-mail: <strong className="color-a" itemprop="email">fmlimao@gmail.com</strong></p>
+            </address>
+        )
+    }
+}
+
 class SiteHeader extends React.Component {
     render() {
         return (
@@ -14,27 +50,15 @@ class SiteHeader extends React.Component {
                     <div className="row">
 
                         <div className="col-xs-5 col-sm-3">
-                            <div className="cv-header-avatar">
-                                <img src={imagePerfil} alt="Leandro Macedo"
-                                    className="img-responsive img-circle img-thumbnail" />
-                            </div>
+                            <SiteHeaderAvatar></SiteHeaderAvatar>
                         </div>
 
                         <div className="col-xs-7 col-sm-5">
-                            <div className="cv-header-titles">
-                                <h1 className="cv-header-title">
-                                    Leandro<br />
-                                    <span className="color-a">Macedo</span>
-                                </h1>
-                                <h2 className="cv-header-subtitle" itemprop="jobTitle">Desenvolvedor</h2>
-                            </div>
+                            <SiteHeaderTitles></SiteHeaderTitles>
                         </div>
 
                         <div className="col-xs-12 col-sm-4">
-                            <address className="cv-header-address">
-                                <p>Telefone: <strong className="color-a" itemprop="telephone">+55 11 98739-1075</strong></p>
-                                <p>E-mail: <strong className="color-a" itemprop="email">fmlimao@gmail.com</strong></p>
-                            </address>
+                            <SiteHeaderAddress></SiteHeaderAddress>
                         </div>
 
                     </div>
